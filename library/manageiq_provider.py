81f0ca82a026f2c3325718966d3e9f91dde16bd1
dkorn <dkorn@redhat.com>
2016-10-09 09:38:50 +0300

    Option to add Amazon EC2 cloud provider to ManageIQ

--- library/manageiq_provider.py
+++ library/manageiq_provider.py
@@ -44,7 +44,7 @@ options:
     description:
       - the provider's type
     required: true
-    choices: ['openshift-origin', 'openshift-enterprise']
+    choices: ['openshift-origin', 'openshift-enterprise', 'amazon']
   state:
     description:
       - the state of the provider
@@ -123,6 +123,19 @@ EXAMPLES = '''
     provider_api_hostname: 'oshift01.redhat.com'
     provider_api_port: '8443'
     provider_api_auth_token: '******'
+
+# Add Amazon EC2 Cloud provider to ManageIQ
+  manageiq_provider:
+    name: 'AWS01'
+    provider_type: 'amazon'
+    provider_region: 'us-west-2"
+    access_key_id: '******'
+    secret_access_key: '******'
+    state: 'present'
+    verify_ssl: False
+    miq_url: 'http://localhost:3000'
+    miq_username: 'admin'
+    miq_password: '******'
 '''
 
 
@@ -139,7 +152,8 @@ class ManageIQ(object):
     OPENSHIFT_DEFAULT_PORT = '8443'
     provider_types = {
         'openshift-origin': 'ManageIQ::Providers::Openshift::ContainerManager',
-        'openshift-enterprise': 'ManageIQ::Providers::OpenshiftEnterprise::ContainerManager'}
+        'openshift-enterprise': 'ManageIQ::Providers::OpenshiftEnterprise::ContainerManager',
+        'amazon': 'ManageIQ::Providers::Amazon::CloudManager'}
 
     def __init__(self, module, url, user, password, verify_ssl, ca_bundle_path):
         self.module        = module
@@ -150,14 +164,14 @@ class ManageIQ(object):
         self.changed       = False
         self.providers_url = self.api_url + '/providers'
 
-    def required_updates(self, provider_id, endpoints, zone_id):
+    def required_updates(self, provider_id, endpoints, zone_id, provider_region):
         """ Checks whether an update is required for the provider
 
         Returns:
-            Empty Hash (None) - If the hostname and port passed equals the
-                                provider's current values
-            Hash of Changes   - Changes that need to be made if any endpoint is
-                                different than the current values of the
+            Empty Hash (None) - If the hostname, port, zone and region passed equals
+                                the provider's current values
+            Hash of Changes   - Changes that need to be made if any endpoint, zone
+                                or region are different than the current values of the
                                 provider. The hash will have three entries:
                                     Updated, Removed, Added
                                 that will contain all the changed endpoints
@@ -169,11 +183,12 @@ class ManageIQ(object):
             self.module.fail_json(msg="Failed to get provider data. Error: %s" % e)
 
         def host_port(endpoint):
-            return {'hostname': endpoint['hostname'], 'port': int(endpoint['port'])}
+            return {'hostname': endpoint.get('hostname'), 'port': endpoint.get('port')}
 
         desired_by_role = {e['endpoint']['role']: host_port(e['endpoint']) for e in endpoints}
         result_by_role = {e['role']: host_port(e) for e in result['endpoints']}
-        if result_by_role == desired_by_role and result['zone_id'] == zone_id:
+        existing_provider_region = result.get('provider_region') or None
+        if result_by_role == desired_by_role and result['zone_id'] == zone_id and existing_provider_region == provider_region:
             return {}
         updated = {role: {k: v for k, v in ep.items()
                           if k not in result_by_role[role] or v != result_by_role[role][k]}
@@ -185,21 +200,24 @@ class ManageIQ(object):
                    if role not in desired_by_role}
         if result['zone_id'] != zone_id:
             updated['zone_id'] = zone_id
+        if existing_provider_region != provider_region:
+            updated['provider_region'] = provider_region
         return {"Updated": updated, "Added": added, "Removed": removed}
 
-    def update_provider(self, provider_id, provider_name, endpoints, zone_id):
+    def update_provider(self, provider_id, provider_name, endpoints, zone_id, provider_region):
         """ Updates the existing provider with new parameters
         """
         try:
             self.client.post('{api_url}/providers/{id}'.format(api_url=self.api_url, id=provider_id),
                              action='edit',
                              zone={'id': zone_id},
-                             connection_configurations=endpoints)
+                             connection_configurations=endpoints,
+                             provider_region=provider_region)
             self.changed = True
         except Exception as e:
             self.module.fail_json(msg="Failed to update provider. Error: %s" % e)
 
-    def add_new_provider(self, provider_name, provider_type, endpoints, zone_id):
+    def add_new_provider(self, provider_name, provider_type, endpoints, zone_id, provider_region):
         """ Adds a provider to manageiq
 
         Returns:
@@ -209,7 +227,8 @@ class ManageIQ(object):
             result = self.client.post(self.providers_url, name=provider_name,
                                       type=ManageIQ.provider_types[provider_type],
                                       zone={'id': zone_id},
-                                      connection_configurations=endpoints)
+                                      connection_configurations=endpoints,
+                                      provider_region=provider_region)
             provider_id = result['results'][0]['id']
             self.changed = True
         except Exception as e:
@@ -234,12 +253,13 @@ class ManageIQ(object):
         providers = self.client.collections.providers
         return next((p.id for p in providers if p.name == provider_name), None)
 
-    def generate_endpoint(self, role, hostname, port, authtype, token):
+    def generate_endpoint(self, role, authtype, hostname=None, port=None, token=None, userid=None, password=None):
         """ Returns one provider endpoint hash.
         """
         return {'endpoint': {'role': role, 'hostname': hostname,
-                             'port': port},
-                'authentication': {'role': authtype, 'auth_key': token}}
+                             'port': port and int(port)},
+                'authentication': {'authtype': authtype, 'auth_key': token,
+                                   'userid': userid, 'password': password}}
 
     def delete_provider(self, provider_name):
         """ Deletes the provider
@@ -264,7 +284,7 @@ class ManageIQ(object):
         else:
             return dict(task_id=None, changed=self.changed, msg="Provider {provider_name} doesn't exist".format(provider_name=provider_name))
 
-    def add_or_update_provider(self, provider_name, provider_type, endpoints, zone):
+    def add_or_update_provider(self, provider_name, provider_type, endpoints, zone, provider_region):
         """ Adds a provider to manageiq or update its attributes in case
         a provider with the same name already exists
 
@@ -278,9 +298,9 @@ class ManageIQ(object):
         provider_id = self.find_provider_by_name(provider_name)
         updates = {}
         if provider_id:  # provider exists
-            updates = self.required_updates(provider_id, endpoints, zone_id)
+            updates = self.required_updates(provider_id, endpoints, zone_id, provider_region)
             if updates:
-                self.update_provider(provider_id, provider_name, endpoints, zone_id)
+                self.update_provider(provider_id, provider_name, endpoints, zone_id, provider_region)
                 message = "Successfuly updated {provider} provider".format(
                     provider=provider_name)
             else:
@@ -293,7 +313,7 @@ class ManageIQ(object):
             )
         else:  # provider doesn't exists, adding it to manageiq
             provider_id = self.add_new_provider(provider_name, provider_type,
-                                                endpoints, zone_id)
+                                                endpoints, zone_id, provider_region)
             return dict(
                 provider_id=provider_id,
                 changed=self.changed,
@@ -307,7 +327,7 @@ def main():
             name=dict(required=True),
             zone=dict(required=False, type='str'),
             provider_type=dict(required=True,
-                               choices=['openshift-origin', 'openshift-enterprise']),
+                               choices=['openshift-origin', 'openshift-enterprise', 'amazon']),
             state=dict(default='present',
                        choices=['present', 'absent']),
             miq_url=dict(default=os.environ.get('MIQ_URL', None)),
@@ -315,16 +335,22 @@ def main():
             miq_password=dict(default=os.environ.get('MIQ_PASSWORD', None)),
             provider_api_port=dict(default=ManageIQ.OPENSHIFT_DEFAULT_PORT,
                                    required=False),
-            provider_api_hostname=dict(required=True),
-            provider_api_auth_token=dict(required=True, no_log=True),
+            provider_api_hostname=dict(required=False),
+            provider_api_auth_token=dict(required=False, no_log=True),
             verify_ssl=dict(require=False, type='bool', default=True),
             ca_bundle_path=dict(required=False, type='str', defualt=None),
+            provider_region=dict(required=False, type='str'),
+            access_key_id=dict(required=False, type='str', no_log=True),
+            secret_access_key=dict(required=False, type='str', no_log=True),
             metrics=dict(required=False, type='bool', default=False),
             hawkular_hostname=dict(required=False),
             hawkular_port=dict(required=False)
         ),
         required_if=[
+            ('provider_type', 'openshift-origin', ['provider_api_hostname', 'provider_api_port', 'provider_api_auth_token']),
+            ('provider_type', 'openshift-enterprise', ['provider_api_hostname', 'provider_api_port', 'provider_api_auth_token']),
             ('metrics', True, ['hawkular_hostname', 'hawkular_port']),
+            ('provider_type', 'amazon', ['access_key_id', 'secret_access_key', 'provider_region'])
         ],
     )
 
@@ -332,32 +358,39 @@ def main():
         if module.params[arg] in (None, ''):
             module.fail_json(msg="missing required argument: {}".format(arg))
 
-    miq_url        = module.params['miq_url']
-    miq_username   = module.params['miq_username']
-    miq_password   = module.params['miq_password']
-    verify_ssl     = module.params['verify_ssl']
-    ca_bundle_path = module.params['ca_bundle_path']
-    provider_name  = module.params['name']
-    provider_type  = module.params['provider_type']
-    state          = module.params['state']
-    zone           = module.params['zone']
-    hostname       = module.params['provider_api_hostname']
-    port           = module.params['provider_api_port']
-    token          = module.params['provider_api_auth_token']
-    h_hostname     = module.params['hawkular_hostname']
-    h_port         = module.params['hawkular_port']
+    miq_url           = module.params['miq_url']
+    miq_username      = module.params['miq_username']
+    miq_password      = module.params['miq_password']
+    verify_ssl        = module.params['verify_ssl']
+    ca_bundle_path    = module.params['ca_bundle_path']
+    provider_name     = module.params['name']
+    provider_type     = module.params['provider_type']
+    state             = module.params['state']
+    zone              = module.params['zone']
+    provider_region   = module.params['provider_region']
+    access_key_id     = module.params['access_key_id']
+    secret_access_key = module.params['secret_access_key']
+    hostname          = module.params['provider_api_hostname']
+    port              = module.params['provider_api_port']
+    token             = module.params['provider_api_auth_token']
+    h_hostname        = module.params['hawkular_hostname']
+    h_port            = module.params['hawkular_port']
 
     manageiq = ManageIQ(module, miq_url, miq_username, miq_password, verify_ssl, ca_bundle_path)
 
     if state == 'present':
-        endpoints = [manageiq.generate_endpoint('default', hostname, port, 'bearer', token)]
-        if module.params['metrics']:
-            endpoints += [manageiq.generate_endpoint('hawkular', h_hostname, h_port, 'hawkular', token)]
+        if provider_type in ("openshift-enterprise", "openshift-origin"):
+            endpoints = [manageiq.generate_endpoint('default', 'bearer', hostname, port, token)]
+            if module.params['metrics']:
+                endpoints += [manageiq.generate_endpoint('hawkular', 'hawkular', h_hostname, h_port, token)]
+        elif provider_type == "amazon":
+            endpoints = [manageiq.generate_endpoint('default', 'default', userid=access_key_id, password=secret_access_key)]
 
         res_args = manageiq.add_or_update_provider(provider_name,
                                                    provider_type,
                                                    endpoints,
-                                                   zone)
+                                                   zone,
+                                                   provider_region)
     elif state == 'absent':
         res_args = manageiq.delete_provider(provider_name)
 
