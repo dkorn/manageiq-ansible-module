048dcb9cb663a1ecd11a56343a6ec52132d42a2a
dkorn <dkorn@redhat.com>
2016-10-30 17:50:35 +0200

    Add SSL Cert Verification support
    SSL certificate verification is enabled by default.
    To use a self-signed certificate pass the certificate file or
    directory path using the ca_bundle_path option:
    `ca_bundle_path: '/path/to/certfile'`.
    
    To ignore verifying the SSL certificate pass
    `verify_ssl: False`

--- tests/test_container_provider.py
+++ tests/test_container_provider.py
@@ -69,7 +69,8 @@ def miq(miq_api_class, miq_ansible_module, the_provider, the_zone):
 
     miq_ansible_module.fail_json = fail
     miq = manageiq_provider.ManageIQ(miq_ansible_module, MANAGEIQ_HOSTNAME,
-                                     "The username", "The password")
+                                     "The username", "The password",
+                                     verify_ssl=False, ca_bundle_path=None)
 
     miq_api_class.return_value.post.return_value = dict(results=[
         {'api_version': u'v1',
