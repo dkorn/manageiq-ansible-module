8e611f048fe7290c347500d028d88ce7cacc627d
dkorn <dkorn@redhat.com>
2016-11-15 18:16:06 +0200

    ManageIQ User Management module

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     url='https://github.com/dkorn/manageiq-ansible-module',
     package_dir={'': 'library'},
     py_modules=["manageiq_provider", "manageiq_policy_assignment",
-                "manageiq_custom_attributes"],
+                "manageiq_custom_attributes", "manageiq_user"],
     install_requires='ansible manageiq-api-client-python'.split(),
 )
